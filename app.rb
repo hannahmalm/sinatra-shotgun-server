44f869439dbf0f78903149bff91fd1c9da9780ff
Hannah Malm <hannahemalm616@gmail.com>
2021-08-20 11:18:09 -0400

    commit tree b702614321fdc34eaf08d9d85edc4c8134e66fc3

--- app.rb
+++ app.rb
@@ -3,7 +3,11 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "Started my server using Shotgun"
   end
 
 end
+
+#to start shotgun run bundle install
+#then run gem install shotgun
+#then run shotgun - see the http on your computer and refresh. 
